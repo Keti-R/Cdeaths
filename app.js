54aba7558087c05dfed6e77548e641e7e6f4e6a7
Keti Ruadze <76898185+Keti-R@users.noreply.github.com>
2021-11-22 18:42:46 +0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,45 +1,44 @@
-const neededFields = {
-    'population': {
-        name: "Population",
-        ff: function(number) {
-            return new Intl.NumberFormat('en-IN', { maximumSignificantDigits: 3 }).format(number)
-        }
-    },
-    'deaths': {
-        name: "Deaths",
-        ff: function(number) {
-            return new Intl.NumberFormat('en-IN', { maximumSignificantDigits: 3 }).format(number)
-        }
-
-    },
-
-};
-
-(async function() {
-    const tableHead = document.getElementById('thead');
-    for (const options of Object.values(neededFields)) {
-        const th = document.createElement('th');
-        th.appendChild(document.createTextNode(options.name));
-        tableHead.appendChild(th);
+let results = []
+const fetchFlags = async() => {
+        await fetch('https://corona.lmao.ninja/v2/countries?yesterday&strict')
+            .then(response => response.json())
+            .then(data => {
+                results = data.map(e => {
+                    return {
+                        country: e.country,
+                        flag: e.countryInfo.flag,
+                        people: e.population,
+                        death: e.deaths,
+                    }
+                })
+            })
+        console.log(results)
     }
-
-    const resp = await fetch('https://corona.lmao.ninja/v2/countries?yesterday&strict');
-    const data = await resp.json();
-
-    const tableBody = document.getElementById('tbody');
-    for (const cdata of data) {
-        const row = document.createElement('tr');
-
-        for (const key of Object.keys(neededFields)) {
-            const cell = document.createElement('td');
-            const options = neededFields[key];
-
-
-            cell.appendChild(document.createTextNode(
-                options.ff(cdata[key])
-            ));
-            row.appendChild(cell);
+    (async function() {
+        const tableHead = document.getElementById('thead');
+        for (const options of Object.values(fetchFlags)) {
+            const th = document.createElement('th');
+            th.appendChild(document.createTextNode(options.name));
+            tableHead.appendChild(th);
         }
-        tableBody.appendChild(row);
-    }
-})()
+        const resp = await fetch('https://corona.lmao.ninja/v2/countries?yesterday&strict')
+        const data = await resp.json()
+        const tableBody = document.getElementById('tbody');
+        for (const cdata of data) {
+            const row = document.createElement('tr');
+
+            for (const key of Object.keys(fetchFlags)) {
+                const cell = document.createElement('td');
+                const options = neededFields[key];
+
+
+                cell.appendChild(document.createTextNode(
+                    options.ff(cdata[key])
+                ));
+                row.appendChild(cell);
+            }
+            tableBody.appendChild(row);
+        }
+
+        fetchFlags();
+    })()
