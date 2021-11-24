2ed163243a2ebac0027dc252b82125a4ec6f4b43
Keti Ruadze <76898185+Keti-R@users.noreply.github.com>
2021-11-24 18:58:31 +0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,45 +1,83 @@
 const neededFields = {
+    'country': {
+        name: "Country",
+        ff: function(country) {
+            return country;
+        }
+    },
     'population': {
         name: "Population",
         ff: function(number) {
-            return new Intl.NumberFormat('en-IN', { maximumSignificantDigits: 3 }).format(number)
+            return Intl.NumberFormat('en-IN', { maximumSignificantDigits: 3 })
+                .format(number)
+        }
+    },
+    'continent': {
+        name: "Continent",
+        ff: function(continent) {
+            return continent;
+        }
+    },
+    'active': {
+        name: "Active",
+        ff: function(active) {
+            return active;
         }
     },
     'deaths': {
         name: "Deaths",
-        ff: function(number) {
-            return new Intl.NumberFormat('en-IN', { maximumSignificantDigits: 3 }).format(number)
+        ff: function(deaths) {
+            return deaths;
         }
-
-    },
-
+    }
 };
 
 (async function() {
     const tableHead = document.getElementById('thead');
-    for (const options of Object.values(neededFields)) {
+    for (const header of Object.values(neededFields)) {
         const th = document.createElement('th');
-        th.appendChild(document.createTextNode(options.name));
+        th.appendChild(document.createTextNode(header.name));
         tableHead.appendChild(th);
     }
 
-    const resp = await fetch('https://corona.lmao.ninja/v2/countries?yesterday&strict%27');
+    const resp = await fetch('https://corona.lmao.ninja/v2/countries?yesterday&strict');
     const data = await resp.json();
 
     const tableBody = document.getElementById('tbody');
     for (const cdata of data) {
+        console.log(cdata);
         const row = document.createElement('tr');
 
         for (const key of Object.keys(neededFields)) {
             const cell = document.createElement('td');
             const options = neededFields[key];
 
-
             cell.appendChild(document.createTextNode(
-                options.ff(cdata[key])
+                neededFields[key].ff(cdata[key])
             ));
             row.appendChild(cell);
         }
+
         tableBody.appendChild(row);
     }
 })()
+
+function filter(num1) {
+    let input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById(num1);
+    filter = input.value.toUpperCase();
+    table = document.getElementById("table");
+    tr = table.getElementsByTagName("tr");
+
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[num1 - 1];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
